ab2a06b881e51c8c2bb6a7ed44c17f3c4e9f33a4
arfian-rp <arfianpradana07@gmail.com>
2021-12-11 14:19:05 +0700

    ok

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,15 @@
 self.addEventListener("install", (e) => {
-  console.log(`Install!`);
+  e.waitUntil(
+    caches.open("static").then((c) => {
+      return c.addAll(["./", "./favicon.ico", "./manifest.json", "./img/icon-192.png", "./src/main.js", "./sw.js"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+      return r || fetch(e.request);
+    })
+  );
 });
