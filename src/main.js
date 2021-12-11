7b2d70483d2328f77ee0371fd62de65209dd6e3b
arfian-rp <arfianpradana07@gmail.com>
2021-12-11 16:26:36 +0700

    ok

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("sw.js")
-    .then((reg) => {
-      console.log(`SW Registered!`);
-      console.log(reg);
-    })
-    .catch((e) => {
-      console.log(`SW Failed!`);
-      console.log(e);
-    });
-}
-
 import { createApp } from "vue";
 import App from "./App.vue";
 import Routes from "./routes";
